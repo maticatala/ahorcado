c076cd717427515265265fce96a62de245c8066d
Matias Catala <matiasncatala@gmail.com>
2025-07-23 18:12:31 -0300

    rompiendo el codigo

--- tests/game.test.mjs
+++ tests/game.test.mjs
@@ -12,27 +12,27 @@ import {
 
 describe('Juego del ahorcado', () => {
     
-    describe('esPalabraCorrecta()', () => {
-        test('test_arriesgar_palabra_igual_a_secreta_retorna_true', () => {
-            const palabraSecreta = 'javascript';
-            const intento = 'javascript';
+    // describe('esPalabraCorrecta()', () => {
+    //     test('test_arriesgar_palabra_igual_a_secreta_retorna_true', () => {
+    //         const palabraSecreta = 'javascript';
+    //         const intento = 'javascript';
 
-            const resultado = esPalabraCorrecta(palabraSecreta, intento);
+    //         const resultado = esPalabraCorrecta(palabraSecreta, intento);
 
-            expect(resultado).toBe(true);
-        })
-    })
+    //         expect(resultado).toBe(true);
+    //     })
+    // })
 
-    describe('esPalabraIncorrecta()', () => {
-        test('test_arriesgar_palabra_distinta_a_secreta_retorna_false', () => {
-            const palabraSecreta = 'javascript';
-            const intento = 'grupo06';
+    // describe('esPalabraIncorrecta()', () => {
+    //     test('test_arriesgar_palabra_distinta_a_secreta_retorna_false', () => {
+    //         const palabraSecreta = 'javascript';
+    //         const intento = 'grupo06';
 
-            const resultado = esPalabraCorrecta(palabraSecreta, intento);
+    //         const resultado = esPalabraCorrecta(palabraSecreta, intento);
 
-            expect(resultado).toBe(false);
-        })
-    })
+    //         expect(resultado).toBe(false);
+    //     })
+    // })
 
   describe('contieneLetra()', () => {
     test('retorna true si la letra está en la palabra', () => {
